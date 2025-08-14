41e3b588a7739814e8683829aef749cf6c30e55b
NexonDEV <liko1q123@gmail.com>
2025-08-14 18:05:18 +0200

    SEO improvements

--- src/seo/SEO.tsx
+++ src/seo/SEO.tsx
@@ -92,8 +92,8 @@ export function TitleSync() {
     useEffect(() => {
         const rec = byRoute[normalize(pathname)]
         document.title = rec?.meta?.title
-            ? `${rec.meta.title} • HostGier.PL Knowledge Base`
-            : 'HostGier.PL Knowledge Base'
+            ? `${rec.meta.title} • HostGier.PL - Baza Wiedzy`
+            : 'HostGier.PL - Baza Wiedzy'
     }, [pathname])
     return null
 }
